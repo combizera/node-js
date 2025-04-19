ed8c104beeac2dc7510aeb609753f99bd277654d
combizera <ycombi349@gmail.com>
2025-04-19 15:14:52 -0300

    feat: init .env

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,15 @@
+import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL not found. Please insert in .env file')
+}
 
 export const config: Knex.Config = {
   client: 'sqlite3',
   useNullAsDefault: true,
   connection: {
-    filename: './database/app.db',
+    filename: process.env.DATABASE_URL,
   },
   migrations: {
     extension: 'ts',

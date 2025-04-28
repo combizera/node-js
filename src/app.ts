67049518190bfbd2ca82a6b76464a5bbfafeedf6
combizera <ycombi349@gmail.com>
2025-04-27 21:53:04 -0300

    feat: implement user registration functionality with bcryptjs for password hashing (Use Case)

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-import { z } from 'zod'
 import fastify from 'fastify'
 
-import { prisma } from './lib/prisma'
+import { appRoutes } from '@/http/routes'
 
 export const app = fastify()
 
@@ -10,23 +9,4 @@ app.get('/hello', () => {
   return 'Hello world!'
 })
 
-// Create User
-app.post('/users', async (request, reply) => {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
-
-  const { name, email, password } = registerBodySchema.parse(request.body)
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  })
-
-  return reply.status(201).send()
-})
+app.register(appRoutes)

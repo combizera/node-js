8303824ea610f2fbbf4379f38348a1809734b541
combizera <ycombi349@gmail.com>
2025-04-28 20:34:49 -0300

    feat: add user existence check in registration and handle error response

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,6 +3,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -22,8 +23,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       email,
       password,
     })
-  } catch {
-    return reply.status(409).send()
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({
+        message: error.message,
+      })
+    }
+
+    // TODO: Melhorar essa bomba aqui
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -4,22 +4,22 @@ import { prisma } from '@/lib/prisma'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
-  async findByEmail(email: string) {
-    const user = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
-
-    return user
-  }
-
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data: {
         name: data.name,
         email: data.email,
         password_hash: data.password_hash,
+      },
+    })
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
       },
     })
 

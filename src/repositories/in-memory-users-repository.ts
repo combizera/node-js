a3fdd5b05eb243970e48e884081541be0f2a313a
combizera <ycombi349@gmail.com>
2025-04-28 20:16:53 -0300

    feat: restructure user repository implementation and update register use case to use UsersRepository interface

--- src/repositories/in-memory-users-repository.ts
+++
@@ -1,9 +0,0 @@
-import { Prisma } from 'generated/prisma'
-
-export class InMemoryUsersRepository {
-  public users: Prisma.UserCreateInput[] = []
-
-  async create(data: Prisma.UserCreateInput) {
-    this.users.push(data)
-  }
-}
---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,28 @@
+import { Prisma } from 'generated/prisma'
+
+import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '../users-repository'
+
+export class PrismaUsersRepository implements UsersRepository {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        password_hash: data.password_hash,
+      },
+    })
+
+    return user
+  }
+}

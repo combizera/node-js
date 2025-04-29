ebdf2f4e1fdcc85c9f2c5767c3c9376a81c57e22
combizera <ycombi349@gmail.com>
2025-04-28 21:06:28 -0300

    feat: global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import fastify from 'fastify'
 
 import { appRoutes } from '@/http/routes'
+import { ZodError } from 'zod'
+import { env } from '@/env'
 
 export const app = fastify()
 
@@ -10,3 +12,22 @@ app.get('/hello', () => {
 })
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to an external tool like DataDog/Sentry
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error.',
+  })
+})
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -30,8 +30,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       })
     }
 
-    // TODO: Melhorar essa bomba aqui
-    return reply.status(500).send()
+    throw error
   }
 
   return reply.status(201).send()

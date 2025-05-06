41fecbbd639abcc4f162e0e38294ae8783aad2f9
combizera <ycombi349@gmail.com>
2025-05-05 22:40:57 -0300

    feat: validate distance between gym and user

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -3,6 +3,7 @@ import { CheckIn } from 'generated/prisma'
 import { GymRepository } from '@/repositories/gyms-repository'
 import { CheckInRepository } from '@/repositories/check-ins-repository'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coodinate'
 
 interface CheckInUseCaseRequest {
   userId: string
@@ -24,6 +25,8 @@ export class CheckInUseCase {
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
     const gym = await this.gymsRepository.findById(gymId)
 
@@ -31,7 +34,19 @@ export class CheckInUseCase {
       throw new ResourceNotFoundError()
     }
 
-    // calculate distance between user and gym
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    )
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new Error()
+    }
 
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
       userId,

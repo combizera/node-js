a391eecc9850c19eaf07d4ac00af92f2505a50f4
combizera <ycombi349@gmail.com>
2025-10-25 17:38:33 -0300

    feat: use-case get user metrics

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,5 +3,6 @@ import { Prisma, CheckIn } from 'generated/prisma'
 export interface CheckInRepository {
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  countByUserId(userId: string): Promise<number>
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
 }

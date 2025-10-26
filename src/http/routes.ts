d751e357857d2a375c4ac52e6b76283e5a669ae0
combizera <ycombi349@gmail.com>
2025-10-26 18:51:03 -0300

    feat: add profile controller

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from 'fastify'
 
+import { profile } from './controllers/profile'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', profile)
 }
